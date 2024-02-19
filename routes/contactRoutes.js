70e59f67ea91751bc53f618a5c07da6edd15d1b4
Mahmoud Lotfy <mahmoud.lotfy.ext@brightskiesinc.com>
2024-02-19 16:15:34 +0200

    Add validateToken middleware to contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -10,6 +10,7 @@ const {
 
 
 const { validateToken } = require("../middleware/validateTokenHandler");
+router.use(validateToken);
 router.route("/").get(getContacts).post(createContact);
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 

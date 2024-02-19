28ecf76204f8f55f9807cdc18b467b61142a18d0
Mahmoud Lotfy <mahmoud.lotfy.ext@brightskiesinc.com>
2024-02-19 16:14:48 +0200

    Update access level into private in contactController.js

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,7 +4,7 @@ const Contact = require('../models/contactModel');
 
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access Public
+//@access Private
 const getContacts = asyncHandler (async (req, res) => {
   const contacts = await Contact.find();
   res.status(200).json(contacts);
@@ -12,7 +12,7 @@ const getContacts = asyncHandler (async (req, res) => {
 
 //@desc Create new contact
 //@route POST /api/contacts
-//@access Public
+//@access Private
 const createContact = asyncHandler(async (req, res) => {
   console.log("The request body is :", req.body);
   const { name, email, phone } = req.body;
@@ -32,7 +32,7 @@ const createContact = asyncHandler(async (req, res) => {
 
 //@desc Get single contact
 //@route GET /api/contacts/:id
-//@access Public
+//@access Private
 const getContact = asyncHandler(async (req, res) => {
 const contact = await Contact.findById(req.params.id);
 if (!contact) {
@@ -45,7 +45,7 @@ if (!contact) {
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
-//@access Public
+//@access Private
 const updateContact = asyncHandler (async (req, res) => {
   const contact = await Contact.findById(req.params.id);
 if (!contact) {
@@ -58,7 +58,7 @@ if (!contact) {
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
-//@access Public
+//@access Private
 const deleteContact = asyncHandler (async (req, res) => {
   const contact = await Contact.findByIdAndDelete(req.params.id);
   if (!contact) {

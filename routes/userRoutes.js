ddc41117cbec10580e896a163b606200506b12a1
asmar2000 <M.L.NAGA@student.aast.edu>
2024-02-19 15:06:32 +0200

    Refactor userRoutes.js to update login and current user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const {registerUser,
-    getUsers,
+    loginUser,
     getCurrentUser} = require("../controllers/userController");
-
-
+const {validateToken} = require("../middleware/validateTokenHandler")
 
 
 
 router.post("/register",registerUser) 
-router.post("/login", getUsers)
-router.get("/current", getCurrentUser)
+router.post("/login", loginUser)
+router.get("/current",validateToken ,getCurrentUser)
 
 
 

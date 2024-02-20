dede4e900abeb6fe81da25678c3b65f5b63adb54
asmar2000 <M.L.NAGA@student.aast.edu>
2024-02-20 14:01:33 +0200

    Add Swagger documentation for API

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
-
+const swaggerjsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 connectDb();
 const app = express();
 
@@ -12,6 +13,30 @@ app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Contact Keeper API",
+      version: "1.0.0",
+      description: "This NodeJS/Express application implements a CRUD (Create, Read, Update, Delete) interface for managing contacts.",
+      contact: {
+        name: "Mahmoud Lotfy",
+        url: "https://github.com/Asmar2000/",
+        email: "mahmoudlotfy3799@gmail.com"
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:5001",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const specs = swaggerjsdoc(options)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
